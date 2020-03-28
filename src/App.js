b9789b56e60aed72a26b19942532671bcc1d5831
Joye Shonubi <joye@omnibiz.com>
2020-03-28 08:07:20 +0100

    installed styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {createGlobalStyle} from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+body{
+  margin: 0;
+}
+`
 
 function App() {
   return (
-    
+    <>
+  <GlobalStyle/>
+    </>
   );
 }
 
