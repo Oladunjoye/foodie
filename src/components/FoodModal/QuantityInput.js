b1b0205a11d46ded9f656d3155849a67f1a1fab7
Joye Shonubi <joye@omnibiz.com>
2020-03-30 03:43:27 +0100

    added increment button to order modal

--- src/components/FoodModal/QuantityInput.js
+++ src/components/FoodModal/QuantityInput.js
@@ -19,17 +19,38 @@ outline: none;
 
 `
 const IncrementContainer = styled(Title)`
-height: 24px
+height: 24px;
 display: flex;
 
+`
+
+const IcrementButton =  styled.div`
+width: 23px;
+color: red;
+text-align: center;
+cursor: pointer;
+line-height: 23px;
+margin: 0px 10px;
+border: 1px solid red;
+${({disabled}) =>  disabled &&
+
+`opacity: 0.5;
+pointer-events: none;
+
+&:hover{
+    background-color: #ffe3e3;
+}
+
+`
+}
 `
 export default function QuantityInput({quantity}) {
     return (
         <IncrementContainer>
-            <div>
-            Quantity : 
-            </div>
-            <QuantityInputStyled {...quantity}></QuantityInputStyled>
+            <div>   Quantity : </div>
+          
+            
+            <QuantityInputStyled {...quantity}/>
        
         </IncrementContainer>
     )
